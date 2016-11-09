dad7727664601e68a464018075a425a7a0306d88
byezy <dbye@vivaldi.net>
2016-11-10 08:31:11 +1100

    fix raster - build RAT

--- tools/raster/calculate_statistics.py
+++ tools/raster/calculate_statistics.py
@@ -1,5 +1,5 @@
 from base.base_tool import BaseTool
-from base.class_decorators import results
+from base.class_decorators import results, geodata
 from base.method_decorators import input_tableview, input_output_table, parameter
 from arcpy import CalculateStatistics_management
 
