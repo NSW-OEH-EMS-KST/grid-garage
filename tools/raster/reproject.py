e24bc1ffd4ed4e78f347aa6e75dd79dfe1293dfc
byezy <dbye@vivaldi.net>
2016-11-14 15:14:47 +1100

    finish raster - reproject tool

--- tools/raster/reproject.py
+++ tools/raster/reproject.py
@@ -1,5 +1,5 @@
 from base.base_tool import BaseTool
-from base.geodata import raster_formats, resample_methods
+from base.geodata import raster_formats, resample_methods, UnknownSrsError
 from base.class_decorators import results, geodata
 from base.method_decorators import input_tableview, input_output_table, parameter
 from arcpy import ProjectRaster_management
@@ -15,7 +15,7 @@ tool_settings = {"label": "Reproject",
 class ReprojectRasterTool(BaseTool):
     def __init__(self):
         BaseTool.__init__(self, tool_settings)
-        self.execution_list = [self.initialise, self.iterating]
+        self.execution_list = [self.initialise, self.iterate]
         self.out_fmt = self.out_cs = self.cellsz = self.resamp = self.rego = None
 
     @input_tableview("raster_table", "Table for Rasters", False, ["raster:geodata:none"])
@@ -31,37 +31,37 @@ class ReprojectRasterTool(BaseTool):
     def initialise(self):
         pd = self.get_parameter_dict()
 
-        self.out_fmt = "" if pd['output_format'].lower == 'esri grid' else pd["output_format"]  # fix output extension
+        self.out_fmt = "" if pd['raster_format'].lower == 'esri grid' else pd["raster_format"]  # fix output extension
         self.out_cs = pd["output_cs"]
-        self.cellsz = pd["cell_size"]
-        self.resamp = pd["resample_type"]
+        self.cellsz = "#" if not pd["cell_size"] else pd['cell_size']
+        self.resamp = "#" if not pd["resample_type"] else pd['resample_type']
         self.rego = "#" if not pd['rego_point'] else pd['rego_point']  # fix empty value for arcgis
 
         return
 
-    def iterating(self):
+    def iterate(self):
         self.iterate_function_on_tableview(self.reproject, "raster_table", ["raster"])
         return
 
     def reproject(self, data):
-        r_in = data['raster']
-        self.send_info(data)
-        d = self.geodata.describe(r_in)
+        ras = data['raster']
+        self.geodata.validate_geodata(ras, raster=True)
+        d = self.geodata.describe(ras)
         r_srs = d.get("dataset_spatialReference", "Unknown")
 
         if "unknown" in r_srs.lower():
-            raise ValueError("Raster dataset '{0}' has unknown spatial reference system ({1})".format(r_in, r_srs))
+            raise UnknownSrsError(ras)
 
-        r_out = self.geodata.make_raster_name(r_in, self.results.output_workspace, self.out_fmt)
+        r_out = self.geodata.make_raster_name(ras, self.results.output_workspace, self.out_fmt)
 
-        tx = self.geodata.get_transformation(r_in, self.out_cs)
+        tx = self.geodata.get_transformation(ras, self.out_cs)
 
-        self.send_info(str([self.out_cs, tx, self.cellsz, self.resamp, self.rego]))
+        # self.send_info(str([self.out_cs, tx, self.cellsz, self.resamp, self.rego]))
 
         # do the business
-        self.send_info("Projecting {0} into {1} -> {2}".format(r_in, r_srs, r_out))
-        ProjectRaster_management(r_in, r_out, self.out_cs, geographic_transform=tx, resampling_type=self.resamp, cell_size=self.cellsz, Registration_Point=self.rego)
+        self.send_info("Projecting {0} into {1} -> {2}".format(ras, r_srs, r_out))
+        ProjectRaster_management(ras, r_out, self.out_cs, geographic_transform=tx, resampling_type=self.resamp, cell_size=self.cellsz, Registration_Point=self.rego)
 
-        self.results.add({"geodata": r_out, "source": r_in, "metadata": "to do"})
+        self.results.add({"geodata": r_out, "source": ras, "metadata": "to do"})
         return
 
