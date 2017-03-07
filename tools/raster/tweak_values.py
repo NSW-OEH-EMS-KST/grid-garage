bade3c644e1e501ac112c16ba8b3b7917022cc7c
byed <david.bye@environment.nsw.gov.au>
2017-03-07 14:10:41 +1100

    raster-tweak values tool refactored -uses logging now ALSO no geodata deco

--- tools/raster/tweak_values.py
+++ tools/raster/tweak_values.py
@@ -1,18 +1,18 @@
 from base.base_tool import BaseTool
-from base.class_decorators import results, geodata
+from base.class_decorators import results
 from base.method_decorators import input_tableview, input_output_table, parameter, raster_formats
-from base.geodata import DoesNotExistError
 from arcpy import Raster, Describe
 from arcpy.sa import Con, Int
 from os.path import join
+from base.utils import validate_geodata, make_raster_name
 
 tool_settings = {"label": "Tweak Values",
-                 "description": "Tweaks...",
+                 "description": "Tweaks raster cell values with simple mathematics and can integerise result",
                  "can_run_background": "True",
                  "category": "Raster"}
 
+
 @results
-@geodata
 class TweakValuesRasterTool(BaseTool):
     def __init__(self):
         BaseTool.__init__(self, tool_settings)
@@ -57,27 +57,32 @@ class TweakValuesRasterTool(BaseTool):
         return
 
     def get_band_nodata(self, raster, bandindex=1):
+        self.log.debug("IN raster={} bandindex= {}".format(raster, bandindex))
+
         d = Describe(join(raster, "Band_{}".format(bandindex)))
         ndv = d.noDataValue
         # v = ap.GetRasterProperties_management(raster, property)
         # self.send_info(type(v))
         # v = v.getOutput(0)
         # print type(v)
+
+        self.log.debug("OUT returning {}".format(ndv))
         return ndv
 
     def tweak(self, data):
-        r_in = data["raster"]
-        if not self.geodata.exists(r_in):
-            raise DoesNotExistError(r_in)
+        self.log.debug("IN data= {}".format(data))
 
-        r_out = self.geodata.make_raster_name(r_in, self.results.output_workspace, self.raster_format)
-        self.send_info("Tweaking raster {0} -->> {1}".format(r_in, r_out))
+        r_in = data["raster"]
+        validate_geodata(r_in, True)
+
+        r_out = make_raster_name(r_in, self.results.output_workspace, self.raster_format)
+        self.log.info("Tweaking raster {0} -->> {1}".format(r_in, r_out))
 
         ras = Raster(r_in)
         vals = []
 
         if self.min_val:
-            self.send_info('Setting minimum {0}...{1}'.format(self.min_val, self.under_min))
+            self.log.info('Setting minimum {0}...{1}'.format(self.min_val, self.under_min))
             under = self.min_val if self.under_min == 'Minimum' else self.get_band_nodata(r_in)  # self.geodata.describe(r_in)["raster_band_noDataValue"]
             # self.send_info(under)
             if under == "#":
@@ -87,7 +92,7 @@ class TweakValuesRasterTool(BaseTool):
             vals.append('MIN {0}...{1} = {2}'.format(self.min_val, self.under_min, under))
 
         if self.max_val:
-            self.send_info('Setting maximum {0}...{1}'.format(self.max_val, self.over_max))
+            self.log.info('Setting maximum {0}...{1}'.format(self.max_val, self.over_max))
             over = self.max_val if self.over_max == 'Maximum' else self.get_band_nodata(r_in)  # self.geodata.describe(r_in)["raster_band_noDataValue"]
             # self.send_info(over)
             if over == "#":
@@ -98,27 +103,28 @@ class TweakValuesRasterTool(BaseTool):
             vals.append('MAX {0}...{1}'.format(self.max_val, self.over_max, over))
 
         if self.scalar:
-            self.send_info('Scaling...{0}'.format(self.scalar))
+            self.log.info('Scaling...{0}'.format(self.scalar))
             ras *= float(self.scalar)
             vals.append('scaled by {0}'.format(self.scalar))
 
         if self.constant:
-            self.send_info('Translating...{0}'.format(self.constant))
+            self.log.info('Translating...{0}'.format(self.constant))
             ras += float(self.constant)
             vals.append('translated by {0}'.format(self.constant))
 
         if self.integerise:
-            self.send_info('Integerising...')
+            self.log.info('Integerising...')
             ras = Int(ras)
             vals.append('integerised (truncation)')
 
         # save and exit
-        self.send_info('Saving to {0}'.format(r_out))
+        self.log.info('Saving to {0}'.format(r_out))
         ras.save(r_out)
 
-        self.results.add({"geodata": r_out, "source_geodata": r_in, "tweaks": ' & '.join(vals)})
+        r = self.results.add({"geodata": r_out, "source_geodata": r_in, "tweaks": ' & '.join(vals)})
+        self.log.info(r)
+
+        self.log.debug("OUT")
         return
 
 # Con (in_conditional_raster, in_true_raster_or_constant, {in_false_raster_or_constant}, {where_clause})
-
-
