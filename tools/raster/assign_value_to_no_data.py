56a696f4b5f6e41eb29f7e55818db60c066df1f1
Byezy <dbye68@gmail.com>
2019-08-12 11:26:41 +1000

    fix assign ndv for integer rasters

--- tools/raster/assign_value_to_no_data.py
+++ tools/raster/assign_value_to_no_data.py
@@ -67,6 +67,21 @@ class AssignValueToNodataRasterTool(BaseTool):
 
         r_out = utils.make_table_name(ras, self.output_file_workspace, self.raster_format, self.output_filename_prefix, self.output_filename_suffix)
 
+        ras = arcpy.Raster(ras)
+        ndv = ras.noDataValue
+        pix_type = ras.pixelType
+
+        self.info("Nodata value is {}".format(ndv))
+
+        if any(x in pix_type for x in ["S", "U"]):
+            self.info("Raster pixel type is '{}' (integer)".format(pix_type))
+            try:
+                self.ndv_value = int(self.ndv_value)
+                self.info("Nodata value cast to integer {}".format(self.ndv_value))
+            except:
+                self.info("New value for NDV cells '{0}' could not be coerced to integer, output raster will be floating point".format(self.ndv_value))
+                pass
+
         self.info("Setting NDV cells on {1} to {0} -> {2}".format(self.ndv_value, ras, r_out))
 
         null_ras = arcpy.sa.IsNull(ras)
